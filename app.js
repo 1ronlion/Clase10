96b578aa5bc49313d53a9933cfb5e535d2697385
1ronlion <leonardo_bana@live.com.ar>
2022-04-06 21:56:33 -0300

    terminado

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require ('path')
 
 const port = 3000
 
+//////////////////////////////////////////////////////////////////
+
+
 app.listen(port, () => console.log("Servidor corriendo en el puerto 3000"))
 
 
@@ -13,12 +16,49 @@ app.listen(port, () => console.log("Servidor corriendo en el puerto 3000"))
 
 app.use(express.static('public'))
 
+//////////////////////////////////////////////////////////////////
 
 
-app.get("/babbage", (req, res) => {
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))   
+})
 
-    res.sendFile(path.join(__dirname, "./views/babbage.html"))
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/babbage.html'))
+})
+
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/berners-lee.html'))
+})
+
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/clarke.html'))
+})
+
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/hamilton.html'))
+})
+
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/hopper.html'))
+})
+
+app.get('/lovelace', (req, res) => {
+	res.sendFile(path.join(__dirname, './views/lovelace.html'))
+})
+
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/turing.html'))
+})
+
+app.get('*', (req, res) => {
+    
+    res.status(404).send("Not found 404")
     
 })
 
+
 ///////////////////////////////////////////////////////////////////
+
+
+
